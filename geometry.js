3d4a129456c08021a2b5394cc4c3f5d06dbd95b8
masud <masudranaacb@gmail.com>
2024-02-15 23:42:00 +0600

    triangle area

--- geometry.js
+++ geometry.js
@@ -1,1 +1,11 @@
-alert("ello")
+
+function getTriangle() {
+    const triangleBase = document.getElementById("triangle_base")
+    const triangleBaseText = parseFloat(triangleBase.value)
+
+    const triangleHeight = document.getElementById("triangle_height")
+    const triangleHeightText = parseFloat(triangleHeight.value)
+    
+    const triangleArea = document.getElementById("triangle_area")
+    triangleArea.innerText = 0.5 * triangleBaseText * triangleHeightText
+}
